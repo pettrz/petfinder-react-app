07bab62715c901784c8c7fb32d283dc70acbfe7c
Petter Knutsson <petterknutsson5@gmail.com>
2018-09-25 05:18:51 +0200

    added eslint plugins

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert("you clicked the title");
-  }
   render() {
     return (
       <div>
-        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
+        <h1>Adopt Me!</h1>
         <Pet name="Luna" animal="dog" breed="Havanese" />
         <Pet name="Pepper" animal="bird" breed="Cockatiel" />
         <Pet name="Doink" animal="cat" breed="Mixed" />
